285b9358d993a3aa204779d9df4e11262546b474
joaopedrodacostacarvalho <jpcc.dev@gmail.com>
2026-03-11 20:16:22 -0300

    Fix rm unused imports

--- src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
@@ -1,12 +1,10 @@
 package br.com.ifpe.oxefood.modelo.entregador;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import jakarta.persistence.Column;
 import jakarta.transaction.Transactional;
 
 @Service
